11fb3e7175701ef019ad8213d05e93da9e28a731
juanvillaman <juandanielvill29@gmail.com>
2025-04-24 14:25:27 -0400

    Testing commit
    Adding JFree program caused problems

--- Project2/jfreechartprograms/src/main/java/com/juanvillaman/jfreechart/TestJFreeCharts.java
+++
@@ -1,9 +0,0 @@
-package com.juanvillaman.jfreechart;
-
-public class TestJFreeCharts {
-  
-  public static void main(String[] args) {
-    JFreeChartsGraphing test = new JFreeChartsGraphing();
-    test.plotGraphs();
-  }
-}
