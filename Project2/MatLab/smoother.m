d4327c9ef8563d8e91c5ca71e2039ee706b89096
juanvillaman <juandanielvill29@gmail.com>
2025-04-24 14:37:48 -0400

    Main Folder Removed
    Still having problems, going to re-commit the project folder

--- Project2/MatLab/smoother.m
+++
@@ -1,9 +0,0 @@
-data = readmatrix('saltedGraph.csv');
-x = data(:, 1);
-y = data(:, 2);
-
-smoothedY = movmean(y, 3);
-smoothedData = [x, smoothedY];
-writematrix(smoothedData, 'smoothedGraph.csv');
-
-disp('Smoothed data saved to smoothedGraph.csv');
