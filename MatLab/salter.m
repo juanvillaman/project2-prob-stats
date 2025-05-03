ae3410a52e94fc3aaf77e96e1cb0bcd747bd0ff2
juanvillaman <juandanielvill29@gmail.com>
2025-05-02 22:15:57 -0400

    MATLAB Documentation Uploaded (work in progress)

--- MatLab/salter.m
+++ MatLab/salter.m
@@ -1,5 +1,5 @@
 % Paste this code into the MatLab Command Window if having trouble running it. It should work perfectfly
-data = readmatrix('quadraticPlot.csv');
+data = readmatrix('Plotter.csv');
 x = data(:, 1);
 y = data(:, 2);
 
@@ -7,9 +7,10 @@ minVal = 1;
 maxVal = 5;
 
 noise = randi([minVal, maxVal], size(y));
-signs = randi([0, 1], size(y)) * 2 - 1;
-saltedY = y + noise .* signs;
+randomSign = randi([0, 1], size(y));
+randomSign(randomSign == 0) = -1;
 
+saltedY = y + noise .* randomSign;
 saltedData = [x, saltedY];
 writematrix(saltedData, 'Salter.csv');
 
